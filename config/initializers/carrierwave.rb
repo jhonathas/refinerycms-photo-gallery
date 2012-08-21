a4115f5c7a1cec0f9ba99d81dae30416ff69e88f
6hoursdaily <jerrypascua@gmail.com>
2012-08-22 06:01:06 +0800

    added Amazon S3 support

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,17 @@
+if (Rails.env.test? || Rails.env.development?)
+  CarrierWave.configure do |config|
+    config.root = Rails.root.to_s + Refinery::PhotoGallery.photo_gallery_dir_relative_to_root
+    #Prefix in Uploader#url See https://github.com/jnicklas/carrierwave/pull/445
+    config.base_path = Refinery::PhotoGallery.photo_gallery_dir
+  end
+else
+  CarrierWave.configure do |config|
+    config.root = Rails.root.join('tmp') 
+    config.cache_dir = 'carrierwave' 
 
-CarrierWave.configure do |config|
-  config.root = Rails.root.to_s + Refinery::PhotoGallery.photo_gallery_dir_relative_to_root
-  #Prefix in Uploader#url See https://github.com/jnicklas/carrierwave/pull/445
-  config.base_path = Refinery::PhotoGallery.photo_gallery_dir
+    config.s3_access_key_id     = ENV['S3_KEY']
+    config.s3_secret_access_key = ENV['S3_SECRET']
+    config.s3_bucket            = ENV['S3_BUCKET']
+    config.s3_access_policy     = "public_read"
+  end
 end
-
