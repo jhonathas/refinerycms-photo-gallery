35590aeeadb24f8a658ebed41236d8fe72c8e57c
6hoursdaily <jerrypascua@gmail.com>
2012-08-23 15:04:38 +0800

    added missing gemspec file

--- lib/refinery/photo_gallery/version.rb
+++ lib/refinery/photo_gallery/version.rb
@@ -3,7 +3,7 @@ module Refinery
     class Version
       @major = 0
       @minor = 0
-      @tiny  = '4-dev'
+      @tiny  = '4.dev'
 
       class << self
         attr_reader :major, :minor, :tiny
